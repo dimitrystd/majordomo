42265309d41d6ee58a45c451b88bce8cc78a9085
Dmitriy Studinskiy <dimitrystd@inbox.ru>
2013-10-22 23:09:49 +0300

    Changed megad for my specific configuration

--- megad.php
+++ megad.php
@@ -1,11 +1,13 @@
 <?php
 
-$op     = 'm';
-$object = 'MyObject';
-$m      = 'MyMethod';
-
-// $params['pt']
+$op='m';
+$object='MegaD1';
+$m='statusChanged';
+/*
+$params['pt']
+*/
 
 include('./objects/index.php');
+
 
 ?>
