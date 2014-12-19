20c4f669a5d516a9b03e818cdf8a8122b8180ad7
Dmitriy Studinskiy <dimitrystd@inbox.ru>
2014-12-20 00:22:40 +0200

    Fixed bug with 'mdid' parameter

--- megad.php
+++ megad.php
@@ -11,22 +11,23 @@ $log = getLogger(__FILE__);
 
 // Try to resolve MegaD object by IP
 $ip = $_SERVER['REMOTE_ADDR'];
-$log->trace(sprintf('Got message from MegaDevice (%s, pt=%s)', $ip, $params['pt']));
-if (!isset($ip)){
+$log->trace(sprintf('Got message from MegaDevice (ip = %s, params = [%s])', $ip, 
+  implode(', ', array_map(function ($v, $k) { return $k . '=' . $v; }, $params, array_keys($params)))));
+if (empty($ip)){
   $log->error('Cannot determinate remote IP address of megadevice!');
   exit;
 }
 $objects = getObjectsByClass('Megadevice');
 $megaD = null;
-if (isset($params['mdid'])) {
+if (!empty($params['mdid'])) {
   foreach ($objects as $obj) {
-    if (trim(getGlobal($obj['TITLE'].'.id')) == $params['mdid']) {
+    if (strnatcasecmp(trim(getGlobal($obj['TITLE'].'.id')), $params['mdid']) == 0) {
       $megaD = $obj;
       break;
     }
   }
 }
-if (!isset($megaD)) {
+if (empty($megaD)) {
   foreach ($objects as $obj) {
     if (trim(getGlobal($obj['TITLE'].'.ipAddress')) == $ip) {
       $megaD = $obj;
@@ -34,7 +35,7 @@ if (!isset($megaD)) {
     }
   }
 }
-if (!isset($megaD)) {
+if (empty($megaD)) {
   $log->error('Cannot find instance of Megadevice class with ip = ' . $ip);
   exit;
 }
@@ -42,13 +43,14 @@ if (!isset($megaD)) {
 $inputDevice = $megaD['TITLE'];
 $inputPort = $params['pt'];
 $objects = getObjectsByClass("Light");
+$light = null;
 foreach ($objects as $obj) {
   if (strnatcasecmp(getGlobal($obj['TITLE'].'.inputDevice'), $inputDevice) == 0 &&
-    strnatcasecmp(getGlobal($obj['TITLE'].'.inputPort'), $inputPort) == 0
-  )
+    strnatcasecmp(getGlobal($obj['TITLE'].'.inputPort'), $inputPort) == 0) {
     $light = $obj;
+  }
 }
-if (!isset($light)) {
+if (empty($light)) {
   $log->error(sprintf('Cannot find instance of Light class with inputDevice = %s and inputPort = %d', $inputDevice, $inputPort));
   exit;
 }
