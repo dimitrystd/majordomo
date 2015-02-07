0f235c4ab020034acc9d9a8a448a226dd66b20ec
Dmitriy Studinskiy <dms@smartling.com>
2015-02-07 23:02:04 +0200

    Added cycle for reading data from electric meter

--- mercury.php
+++ mercury.php
@@ -5,52 +5,52 @@
 
 // ini_set('max_execution_time', 10);
 
-// Parameters for port
-exec('/bin/stty -F /dev/ttyUSB0 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts');
+function getDataFromMercury() {
+	// Parameters for port
+	exec('/bin/stty -F /dev/ttyUSB0 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts');
 
-// Open port
-$fp = fopen('/dev/ttyUSB0', 'r+');
+	// Open port
+	$fp = fopen('/dev/ttyUSB0', 'r+');
 
-if (!$fp) {
-  echo 'Error1';
-  exit;
+	if (!$fp) {
+	  echo 'Could not open com port';
+	  exit;
+	}
+
+	$result = '';
+	try {
+		// Sent command to device
+		stream_set_blocking($fp, 1);
+
+		fwrite($fp, "\x00\x0D\x29\x0F\x63\xb2\xbd"); // string to receiving current amperage,voltage with corrected CRC and device address
+
+		// Read answer from device with 500ms timeout
+		stream_set_blocking($fp, 0);
+		$timeout = microtime(1) + 0.5;
+
+		while (microtime(1) < $timeout) {
+		  $c = fgetc($fp);
+		  if ($c === false) {
+		    usleep(5);
+		    continue;
+		  }
+
+		  $result.= $c;
+		}
+	} finally {
+	  fclose($fp);
+	}
+
+	// split answer data on parts
+	// $crc = substr($result, -2); // crc16  of answer
+	// $addr = hexdec(bin2hex(substr($result, 1, 3))); // address of power device
+	// $answer_cmd = substr($result, 4, -2); // answered command
+	$answer = substr($result, 5, -2); // answer string
+
+	// Format and output data
+	$voltage = bin2hex(substr($answer, 0, 2)) / 10;
+	$amperage = bin2hex(substr($answer, 2, 2)) / 100;
+	$energy = bin2hex(substr($answer, 4, 3)) / 1000;
+
+	return array('ac_v'=>$voltage,'ac_i'=>$amperage,'ac_p'=>$energy);
 }
-
-// Sent command to device
-stream_set_blocking($fp, 1);
-
-fwrite($fp, "\x00\x0D\x29\x0F\x63\xb2\xbd"); // string to receiving current amperage,voltage with corrected CRC and device address
-
-// Read answer from device with 500ms timeout
-$result = '';
-$c = '';
-stream_set_blocking($fp, 0);
-$timeout = microtime(1) + 0.5;
-
-while (microtime(1) < $timeout) {
-  $c = fgetc($fp);
-  if ($c === false) {
-    usleep(5);
-    continue;
-  }
-
-  $result.= $c;
-}
-
-fclose($fp);
-
-// split answer data on parts
-// $crc = substr($result, -2); // crc16  of answer
-// $addr = hexdec(bin2hex(substr($result, 1, 3))); // address of power device
-// $answer_cmd = substr($result, 4, -2); // answered command
-$answer = substr($result, 5, -2); // answer string
-
-// Format and output data
-$voltage = bin2hex(substr($answer, 0, 2)) / 10;
-$amperage = bin2hex(substr($answer, 2, 2)) / 100;
-$energy = bin2hex(substr($answer, 4, 3)) / 1000;
-
-echo "<br>Напряжение сети:  $voltage Uv\n";
-echo "<br>Сила така :  $amperage Ia\n";
-echo "<br>Потребляемая мощьность : $energy P kVt";
-		
---
+++ scripts/cycle_mercurypower.php
@@ -0,0 +1,50 @@
+<?php
+/*
+* Снятие показания с счётчика Меркурий 200
+* https://github.com/mrkrasser/MercuryStats/blob/master/Readme.ru.md
+* https://github.com/mrkrasser/MercuryStats/blob/master/examples/MercuryStatsGetCurrent.xively.php
+*/
+
+chdir(dirname(__FILE__).'/../');
+
+include_once("./config.php");
+include_once("./lib/loader.php");
+include_once("./lib/threads.php");
+
+set_time_limit(0);
+
+// connecting to database
+$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
+
+include_once("./load_settings.php");
+require_once("./mercury.php");
+
+$checked_time=0;
+
+echo date("H:i:s") . " running " . basename(__FILE__) . "\n";
+$log = getLogger(__FILE__);
+
+while(1)
+{
+  if (time()-$checked_time>20) {
+    $data = getDataFromMercury();
+    setGlobal('Mercury.Uv', $data['ac_v']);
+    setGlobal('Mercury.Ia', $data['ac_i']);
+    setGlobal('Mercury.Pv', $data['ac_p']);
+    $log->trace("Immediate values of V = ${data['ac_v']}, I = ${data['ac_i']}, P = ${data['ac_p']}");
+    $checked_time=time();
+    setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
+  }
+
+
+  if (file_exists('./reboot') || $_GET['onetime'])
+  {
+    $db->Disconnect();
+    exit;
+  }
+
+  sleep(1);
+}
+
+DebMes('Unexpected close of cycle: ' . basename(__FILE__));
+$log->error('Unexpected close of cycle: ' . basename(__FILE__));
