b9a901dded5cdd096c8df3050268986c3c7299ad
Dmitriy Studinskiy <dms@smartling.com>
2016-05-01 19:34:14 +0300

    Replaced debmes with logger

--- lib/common.class.php
+++ lib/common.class.php
@@ -405,7 +405,7 @@
    $url=BASE_URL.'/objects/?job='.$jobs[$i]['ID'];
    $result=trim(getURL($url, 0));
    if ($result!='OK') {
-    DebMes("Error executing job ".$jobs[$i]['TITLE']." (".$jobs[$i]['ID']."): ".$result);
+     getLogger(__FILE__)->error(sprintf('Error executing job %s (%s): %s', $jobs[$i]['TITLE'], $jobs[$i]['ID'], $result));
    }
   }
  }
